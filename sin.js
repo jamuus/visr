69aced86242d0f46b05c5df8f6f4250ed89c554c
Jam <jamessewart@gmail.com>
2016-06-03 15:09:19 +0100

    add a bunch o graphs

--- sin.js
+++ sin.js
@@ -1,10 +1,8 @@
 var i = 0;
 setInterval(() => {
-    var s = Math.sin(++i / 10);
+    var s = Math.sin(++i / 100);
     console.log('#a:', s);
     console.log('#b:', s * s);
     console.log('#c:', s * s * s);
-    console.log('#d:', s * s * s);
-    console.log('#e:', s * s * s);
-    console.log('#f:', s * s * s);
-}, 1000 / 30);
+    console.log('#d:', s * s * s * s);
+}, 1000 / 60);
